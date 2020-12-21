fed82153564554df99b1618d5166cc14ceba3081
Nicolas <atelando14@gmail.com>
2020-12-21 19:41:34 +0200

    add 'for.cpp'

---
+++ for.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream> 
+#include <iomanip> 
+#include "windows.h"
+
+using namespace std;
+
+int main()
+{
+	
+
+	double z, y, sum = 0;
+	cout << "Enter y = ";
+	cin >> y;
+	for (int m = 1; m < 6; m++)
+	{
+		sum = sum + sin(m);
+	}
+	z = sum + pow(y, 5);
+	cout << "z = " << z;
+}
+
